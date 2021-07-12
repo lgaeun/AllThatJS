5fa0d2a61da15d2a329d6496b31c16da76377125
lgaeun <gaeungaeun4837@gmail.com>
2021-07-12 23:50:48 +0900

    ✨class note 생성, ts basic types

---
+++ React/learn-ts/class-note/1_type_basics.ts
@@ -0,0 +1,9 @@
+//JS
+let str_js = 'hello';
+
+//TS
+let str_ts: string = 'hell0';
+let num: number = 0;
+let arr: Array<number> = [1,2,3];
+let heros: Array<string> = ['ironman','captin','hulk'];
+let item: number[] = [1,2,3]
