604cd85c7ab1e649be880840f39b1a5118cf054c
lgaeun <gaeungaeun4837@gmail.com>
2021-07-22 20:47:55 +0900

    style: classnames 라이브러리 bind기능 사용

--- React/react-style/src/components/CheckBox.js
+++ React/react-style/src/components/CheckBox.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {MdCheckBox, MdCheckBoxOutlineBlank} from 'react-icons/md';
 import styles from './CheckBox.module.css';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 function CheckBox({children, checked, ...rest}){
     return (
-      <div className={styles.checkbox}>
+      <div className={cx('checkbox')}>
         <label>
           <input type="checkbox" checked={checked} {...rest} />
-          <div className={styles.icon}>
+          <div className={cx('icon')}>
             {checked ? (
-              <MdCheckBox className={styles.checked} />
+              <MdCheckBox className={cx('checked')} />
             ) : (
               <MdCheckBoxOutlineBlank />
             )}
@@ -24,3 +27,11 @@ export default CheckBox;
 
 // css module 여러 개를 사용할 경우
 // ${styles.one} ${styles.two} 처럼
+
+// classnames bind기능 - 여러 개의 css 클래스 사용 or 조건부 스타일링 시
+// - cx('클래스이름')
+// - cx('one', 'two')
+// - cx('my-component', {
+//     condition: true
+// })
+// - cx('my-component', ['another', 'classnames'])
